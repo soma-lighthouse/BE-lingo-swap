47d85caa2d1f98e2c3dbd1f66fa97fd45ac45ec7
John Doe <ahj9758@naver.com>
2023-07-24 21:26:32 +0900

    [LH-57] Create entity Interest and PreferredInterests(#4)

---
+++ src/main/java/com/lighthouse/lingoswap/member/entity/Interests.java
@@ -0,0 +1,25 @@
+package com.lighthouse.lingoswap.member.entity;
+
+import com.lighthouse.lingoswap.common.entity.DateBasicEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Interests extends DateBasicEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category category;
+
+    private String name;
+}
---
+++ src/main/java/com/lighthouse/lingoswap/member/entity/PreferredInterests.java
@@ -0,0 +1,29 @@
+package com.lighthouse.lingoswap.member.entity;
+
+import com.lighthouse.lingoswap.common.entity.DateBasicEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class PreferredInterests extends DateBasicEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Interests interests;
+
+    private boolean isValid;
+}
