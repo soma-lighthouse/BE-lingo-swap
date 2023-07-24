b61bc4b94b210295ba7726c6d4996e3ca5d4513f
John Doe <ahj9758@naver.com>
2023-07-24 18:45:58 +0900

    [LH-57] Create entity UsedLanguage(#4)

---
+++ src/main/java/com/lighthouse/lingoswap/member/entity/UsedLanguage.java
@@ -0,0 +1,30 @@
+package com.lighthouse.lingoswap.member.entity;
+
+import com.lighthouse.lingoswap.common.entity.DateBasicEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UsedLanguage extends DateBasicEntity {
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        private Member member;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        private Language language;
+
+        private int level;
+
+        private boolean isValid;
+}
