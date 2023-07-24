d671f82652ed1ff4ad976947be02b00e3e97e501
John Doe <ahj9758@naver.com>
2023-07-24 17:00:22 +0900

    [LH-57] Create entity Device (#4)

---
+++ src/main/java/com/lighthouse/lingoswap/member/entity/Device.java
@@ -0,0 +1,32 @@
+package com.lighthouse.lingoswap.member.entity;
+
+import com.lighthouse.lingoswap.common.entity.DateBasicEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Device extends DateBasicEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    private String deviceId;
+
+    private int region;
+
+    private String timezone;
+
+    private String os;
+}
