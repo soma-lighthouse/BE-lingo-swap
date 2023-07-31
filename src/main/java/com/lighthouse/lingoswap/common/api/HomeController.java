2437ee4e6f9ebd3e5240ee141c397cbc6fa3f4dc
YoungJun-L <dudwns328@naver.com>
2023-07-31 09:12:08 +0900

    [LH-51] Rename controller to api

--- src/main/java/com/lighthouse/lingoswap/common/api/HomeController.java
+++ src/main/java/com/lighthouse/lingoswap/common/api/HomeController.java
@@ -1,4 +1,4 @@
-package com.lighthouse.lingoswap.common.controller;
+package com.lighthouse.lingoswap.common.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
