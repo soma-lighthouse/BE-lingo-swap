70fd6eda464a4c70cba6ab59128fc46e6a3988ee
YoungJun-L <dudwns328@naver.com>
2023-08-01 09:32:24 +0900

    [LH-51] Add method to create dto from entity

--- src/main/java/com/lighthouse/lingoswap/member/dto/MemberProfileResponse.java
+++ src/main/java/com/lighthouse/lingoswap/member/dto/MemberProfileResponse.java
@@ -1,10 +1,22 @@
 package com.lighthouse.lingoswap.member.dto;
 
+import com.lighthouse.lingoswap.member.entity.Member;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
-public record MemberProfileResponse(String imageUrl,
+public record MemberProfileResponse(String profileImage,
                                     String name,
-                                    String description, String region,
-                                    List<LanguageResponse> language) {
+                                    String description,
+                                    String region,
+                                    List<MemberLanguageResponse> memberLanguageResponses) {
 
+    public static MemberProfileResponse from(Member member) {
+        return new MemberProfileResponse(member.getProfileImage(),
+                member.getName(),
+                member.getDescription(),
+                member.getRegion(),
+                member.getUsedLanguages().stream().map(MemberLanguageResponse::from).collect(Collectors.toList())
+        );
+    }
 }
