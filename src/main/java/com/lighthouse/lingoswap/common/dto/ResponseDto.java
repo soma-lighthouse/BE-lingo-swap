facd9f9abae6dd4d6ed365edebba7378e2fb50ec
YoungJun-L <dudwns328@naver.com>
2023-08-01 10:45:25 +0900

    [LH-51] Refactor constructors

--- src/main/java/com/lighthouse/lingoswap/common/dto/ResponseDto.java
+++ src/main/java/com/lighthouse/lingoswap/common/dto/ResponseDto.java
@@ -6,18 +6,12 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+@Builder
 @Getter
 public class ResponseDto<T> {
 
     private final LocalDateTime timestamp = LocalDateTime.now(ZoneOffset.UTC);
-    private final String code;
+    private final Integer code;
     private final String message;
     private final T data;
-
-    @Builder
-    public ResponseDto(final String code, final String message, final T data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
 }
