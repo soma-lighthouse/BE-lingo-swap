397f94f7aa9bbd2536f72515a12c6ad2580ce409
YoungJun-L <dudwns328@naver.com>
2023-08-21 13:48:48 +0900

    [LH-157] Fix wrong sql

--- src/main/java/com/lighthouse/lingoswap/member/repository/MemberRepository.java
+++ src/main/java/com/lighthouse/lingoswap/member/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @Query("select m from Member m join fetch m.region join fetch m.usedLanguages u join fetch u.language where m.id in :ids")
+    @Query("select m from Member m join fetch m.region left join fetch m.usedLanguages u left join fetch u.language where m.id in :ids")
     List<Member> findAllByIdsWithRegionAndUsedLanguage(List<Long> ids);
 
     @Query("select m from Member m join fetch m.region join fetch m.usedLanguages u join fetch u.language where m.id = :id")
