73e270224463ebce3624ffc45c86f038393d119c
HyeonJun An <ahj9758@naver.com>
2023-09-22 02:40:53 +0900

    [LH-195] batch scheduler 미적용 (#43)

--- src/main/java/com/lighthouse/lingoswap/LingoSwapApplication.java
+++ src/main/java/com/lighthouse/lingoswap/LingoSwapApplication.java
@@ -3,10 +3,8 @@ package com.lighthouse.lingoswap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableJpaAuditing
 public class LingoSwapApplication {
 
