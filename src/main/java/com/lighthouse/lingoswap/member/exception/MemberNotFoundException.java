f693203bebd10d5a77d52b00381f7adceb834e2e
YoungJun-L <dudwns328@naver.com>
2023-09-22 18:13:15 +0900

    [LH-188] Fix exception message

--- src/main/java/com/lighthouse/lingoswap/member/exception/MemberNotFoundException.java
+++ src/main/java/com/lighthouse/lingoswap/member/exception/MemberNotFoundException.java
@@ -2,7 +2,7 @@ package com.lighthouse.lingoswap.member.exception;
 
 public class MemberNotFoundException extends RuntimeException {
 
-    private static final String MESSAGE = "Couldn't find user with id: %s";
+    private static final String MESSAGE = "Couldn't find user with uuid: %s";
 
     public MemberNotFoundException(final String uuid) {
         super(MESSAGE.formatted(uuid));
